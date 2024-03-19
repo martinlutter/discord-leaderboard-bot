81a0197ee44ebf02a5f8ed80c5bd9cc7b5cb0f7c
Martin Lutter <martin.lutter@everlution.sk>
2024-03-19 23:52:23 +0100

    update packages & import API from the correct package

--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,14 @@
 import { type APIGatewayProxyEvent, type APIGatewayProxyResult, type Context } from 'aws-lambda'
-import dotenv from 'dotenv'
 import { InteractionResponseType, InteractionType, verifyKey } from 'discord-interactions'
 import { type APIChatInputApplicationCommandInteraction, type APIApplicationCommandInteractionDataStringOption } from 'discord-api-types/v10'
 import { REST } from '@discordjs/rest'
-import { API } from '@discordjs/core'
+import { API } from '@discordjs/core/http-only'
 
-dotenv.config()
 const discordToken = process.env.DISCORD_TOKEN!
 const publicKey = process.env.APPLICATION_PUBLIC_KEY!
+
+const rest = new REST({ version: '10' }).setToken(discordToken)
+const api = new API(rest)
 
 export const handler = async (event: APIGatewayProxyEvent, context: Context): Promise<APIGatewayProxyResult> => {
   const lowercaseHeaders = Object.fromEntries(
@@ -50,12 +51,8 @@ export const handler = async (event: APIGatewayProxyEvent, context: Context): Pr
     }
 
     if (interaction.data.name === 'user') {
-      // const rest = new REST({ version: '10' }).setToken(discordToken)
-      // const api = new API(rest)
-
       const target = (interaction.data.options?.find(option => option.name === 'target') as APIApplicationCommandInteractionDataStringOption | null)?.value
       console.log(target)
-      // const userResponse = await api.users.get(target!)
       const user = interaction.data.resolved?.users?.[target!]
       if (!user) {
         return {
